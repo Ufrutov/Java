8a71c13f006d777970c66b67ee3ef4a6bf7e009a
ufrutov <mailluifruti@mail.ru>
2015-10-06 00:08:04 +0300

    Commit homework

---
+++ HelloJava/HomeWork/collections/ArrayList_hw.java
@@ -0,0 +1,37 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ArrayList_hw {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> values = new ArrayList<Integer>(Arrays.asList(0, 1, -2, -3, 4, -5, -6, 7, 8, -9, 10, 11, 12, -13, 14, -15, 16, 17, 18, -19, 20, -21, 22, -23, 24, -25, 26, 27, -28, -29));
+		ArrayList<Integer> great = new ArrayList<Integer>();
+		ArrayList<Integer> less = new ArrayList<Integer>();
+		
+		Iterator<Integer> itr = values.iterator();
+		
+		System.out.println("Original collection ["+values.size()+"]:");
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+" ");
+			if( itr.next() >= 0 )
+				great.add(itr.next());
+			else
+				less.add(itr.next());
+		}
+		
+		itr = great.iterator();
+		System.out.println("\nGreat than zero collection ["+great.size()+"]:");
+		while(itr.hasNext())
+			System.out.print(itr.next()+" ");
+		
+		itr = less.iterator();
+		System.out.println("\nLess than zero collection ["+less.size()+"]:");
+		while(itr.hasNext())
+			System.out.print(itr.next()+" ");
+		
+	}
+
+}
