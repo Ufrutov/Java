c7253f3c99740d27365f616421df59e5b5901e23
USER <USER@c4pc2>
2015-10-27 20:33:01 +0200

    Commit Try/Catch exceprions

---
+++ Exceptions/src/catches/AppMain.java
@@ -0,0 +1,46 @@
+package catches;
+
+import java.util.Scanner;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		System.out.println("*** Subscribe email application ***");
+		Scanner in = new Scanner(System.in);
+		String input;
+		
+		Boolean loop = true;
+		while(loop) {
+			System.out.println("Enter You email");
+			try{
+				input = in.nextLine();
+				
+				if( input.length() < 5 ) throw new Exception("Too short input");
+				if( input.indexOf("@") == -1 ) throw new Exception("Wrong email format");
+				
+				System.out.println("Succesfully subscribed "+input+"!");
+				loop = false;
+				
+			} catch(Exception e) {
+				System.out.println("[E] "+e.getMessage());
+			}
+		}
+		loop = true;
+		while(loop) {
+			System.out.println("Enter You password");
+			try {
+				input = in.nextLine();
+				if( input.length() < 5 ) throw new Exception("Too short input");
+				
+				System.out.println("Logged!");
+				loop = false;
+				
+			} catch(Exception e) {
+				System.out.println("[E] "+e.getMessage());
+			}
+		}
+		
+		in.close();
+
+	}
+}
